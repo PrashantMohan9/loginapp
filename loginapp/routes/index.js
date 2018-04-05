5c8859098c6a342d134b0abd73377d73b14dca6a
Vaibhav Singh <singh.vaibhav32@gmail.com>
2018-04-06 00:11:13 +0530

    loginapp

---
+++ loginapp/routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/rejister
+router.get('/rejister', function(req, res) {
+    res.render('rejister');
+});
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
